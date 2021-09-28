c1fff3dd00f7ae6d2acbd5683eb5591cb2619807
MariaPawlowska <90530451+MariaPawlowska@users.noreply.github.com>
2021-09-28 14:01:24 +0200

    Add files via upload

---
+++ 28.09.2021.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Figury geometryczne</title>
+  </head>
+  <body>
+  <h3>figury geometryczne</h3>
+  <form action="./scripts/4_script.php" method="post">
+    <input type="text" name="name" placecholder="podaj imie"><br><br>
+      <input type="radio" name="rectangleFigure" value="kwadrat" checked>Kwadrat<br>
+      <input type="radio" name="rectangleFigure" value="prostokat">Prostokat<br><br>
+      <input type="submit" value="zatwierdz figure">
+    </from>
+  </body>
+  </html>
